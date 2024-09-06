180e0fb7e6a5527ae31b911060071a52dcbfe4f2
Lex <166012830+xelvhk@users.noreply.github.com>
2024-09-06 14:18:01 +0000

    added status keyboard

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,13 +1,14 @@
 from aiogram import Bot, F, Router
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
-from keyboards.keyboards import volk_kb
+from aiogram.types import Message,reply_keyboard_remove
+from keyboards.keyboards import volk_kb, ans_kb
 from lexicon.lexicon_ru import LEXICON_RU
 from services.services import get_joke
 
 router = Router()
 bot = Bot
 
+user_data = {}
 
 # Этот хэндлер срабатывает на команду /start
 @router.message(CommandStart())
@@ -22,7 +23,7 @@ async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])
 
 
-# Вывод меню шуток и контроля присутствия
+# Вывод шуток и контроль присутствия
 @router.message(F.text.in_([LEXICON_RU['joke']]))
 async def send_joke(message: Message):
     joke = get_joke()
@@ -30,4 +31,13 @@ async def send_joke(message: Message):
 
 @router.message(F.text.in_([LEXICON_RU['mark']]))
 async def weather_command(message: Message):
-    await message.reply(text=LEXICON_RU['mark'])
+    await message.reply(text=LEXICON_RU['choice'],
+                        reply_markup=ans_kb)
+
+# @router.message(F.data)
+# async def handle_status(message: Message):
+#     user_id = message.from_user.id
+#     username = message.from_user.username
+#     status = message.text
+#     user_data[user_id] = {"username": username, "status": status}
+#     await message.reply(f"Ваш статус:{status} записан",reply_markup=reply_keyboard_remove())
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,InlineKeyboardButton
 from lexicon.lexicon_ru import LEXICON_RU
 
 
@@ -12,3 +12,30 @@ volk_kb = ReplyKeyboardMarkup(
               [volk_button_2]],
     resize_keyboard=True
 )
+
+
+ans_button_1 = KeyboardButton(text=LEXICON_RU['place'])
+ans_button_2 = KeyboardButton(text=LEXICON_RU['uvs'])
+ans_button_3 = KeyboardButton(text=LEXICON_RU['uvc'])
+ans_button_4 = KeyboardButton(text=LEXICON_RU['mk'])
+ans_button_5 = KeyboardButton(text=LEXICON_RU['sick'])
+ans_button_6 = KeyboardButton(text=LEXICON_RU['vacation'])
+
+# Создаем клавиатуру функций
+ans_kb = ReplyKeyboardMarkup(
+    keyboard=[[ans_button_1],
+              [ans_button_2],
+              [ans_button_3],
+              [ans_button_4],
+              [ans_button_5],
+              [ans_button_6]],
+    resize_keyboard=True
+)
+
+# builder = InlineKeyboardBuilder()
+# builder.button(text="Я на работе!", callback_data="checkin")
+# builder.button(text="УВС", callback_data="uvs")
+# builder.button(text="УВЦ", callback_data="uvc")
+# builder.button(text="МК", callback_data="mk")
+# builder.button(text="Болею", callback_data="sick")
+# builder.button(text="Отпуск", callback_data="vacation")
